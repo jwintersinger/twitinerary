2ca63f34a7af78b913510c358a1626c7e6654913
Jeff Wintersinger <jeff.git@wintersinger.com>
2009-09-04 21:11:06 +0000

    Fixed forgotten modifications to user view from refactoring.

--- twitinerary/views/user.py
+++ twitinerary/views/user.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 # Should be in its own separate application, perhaps, but this is the only user account-related view
 # I have at the moment, and so its presence here is acceptable.
@@ -6,4 +7,4 @@ def logout(request):
   # If user unauthenticated, he will not be stored in the session.
   if request.user.is_authenticated():
     del request.session['user']
-  return HttpResponseRedirect(reverse(home))
+  return HttpResponseRedirect(reverse('twitinerary.views.tweets.home'))
