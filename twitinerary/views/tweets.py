855f64df840e0bd76e9d890f6b932b1eb16335d6
Jeff Wintersinger <jeff.git@wintersinger.com>
2009-09-13 03:28:37 +0000

    Changed location of various views for cleaner, more logical layout.

--- twitinerary/views/tweets.py
+++ twitinerary/views/tweets.py
@@ -55,25 +55,3 @@ def delete(request):
     tweet.delete()
   return HttpResponseRedirect(reverse(view))
 
-def oauth(request):
-  client = ohauth.TwitterClient(settings.OAUTH_CONSUMER_KEY,
-                            settings.OAUTH_CONSUMER_SECRET,
-                            settings.USER_AGENT)
-
-  if ('oauth_token' in request.GET) and ('oauth_verifier' in request.GET):
-    request_token = request.GET.get('oauth_token')
-    verifier = request.GET.get('oauth_verifier')
-    access_token, access_secret = client.get_access_token(request_token, verifier)
-    response = client.fetch('/account/verify_credentials.json', access_token=access_token,
-                            access_secret=access_secret)
-
-    user_info = json.loads(response.content)
-    username = user_info['screen_name']
-    user = AuthenticatedUser.get_or_insert(key_name      = username,
-                                           username      = username,
-                                           access_token  = access_token,
-                                           access_secret = access_secret)
-    request.session['user_key'] = user.key()
-    return HttpResponseRedirect(reverse(home))
-  else:
-    return HttpResponseRedirect( client.get_authorization_url(request.build_absolute_uri()) )
--- twitinerary/views/user.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
-
-def logout(request):
-  if 'user_key' in request.session:
-    del request.session['user_key']
-  return HttpResponseRedirect(reverse('twitinerary.views.tweets.home'))
