32ef4f6a90b2bce608d5c281db19652fb1aef35c
Jeff Wintersinger <jeff.git@wintersinger.com>
2009-09-19 20:34:01 +0000

    Prevent "too many Tweets sent" error from being triggered when editing Tweet. Prevent editing and deletion of Tweets that have already been Tweeted.

--- twitinerary/views/tweets.py
+++ twitinerary/views/tweets.py
@@ -5,23 +5,16 @@ from django.views.generic.simple import direct_to_template
 from google.appengine.ext.db import BadKeyError
 from twitinerary.models import ScheduledTweet, Twitterer
 
+
+#=======
+# Views
+#=======
 def home(request):
   tweets = ScheduledTweet.all()
   tweets.order('-post_at')
   response = direct_to_template(request, 'home.html', {'tweets': tweets})
   _indicate_twitter_password_stored(request.user, response)
   return response
-
-def _indicate_twitter_password_stored(user, response):
-  if user.password:
-    # The same cookie is used by image_uploader.js, so remember to change its name
-    # and value there if you do so here, and vice versa. Don't worry about
-    # deleting it or setting it for beyond the browsing session -- both tasks are
-    # handled in Javascript. This is only to prevent the user from being prompted
-    # for his password in the event that his password is stored in the datastore,
-    # but the cookie indicating such has not been set by prior execution of the
-    # associated Javascript.
-    response.set_cookie('twitter_password_stored', 'true')
 
 def new(request):
   return direct_to_template(request, 'new_tweet.html')
@@ -34,15 +27,15 @@ def save(request):
   user = request.user
   if not user.is_authenticated():
     return HttpResponse('Not authenticated.', status=401, content_type='text/plain')
-  if _too_many_tweets_sent(user):
-    return HttpResponse('Too many Tweets sent.', status=403, content_type='text/plain')
 
   key = request.POST.get('key')
   if key:
-    tweet = ScheduledTweet.get(key)
-    if tweet.user != request.user:
-      raise Exception('User does not own Tweet')
+    tweet = _get_untweeted_tweet(key, user)
   else:
+    # Only check if user exceeded usage limits when new Tweet created, not when
+    # editing existing one.
+    if _too_many_tweets_sent(user):
+      return HttpResponse('Too many Tweets sent.', status=403, content_type='text/plain')
     tweet = ScheduledTweet()
 
   properties = {'user':       user,
@@ -52,11 +45,6 @@ def save(request):
   [setattr(tweet, property, properties[property]) for property in properties]
   tweet.put()
   return HttpResponse('Woohoo! Your Tweet has been scheduled.', content_type='text/plain')
-
-def _too_many_tweets_sent(user):
-  tweets = ScheduledTweet.all()
-  tweets.filter('user =', user)
-  return tweets.count() >= 5
 
 def view(request):
   if not request.user.is_authenticated():
@@ -69,11 +57,37 @@ def view(request):
   return direct_to_template(request, 'view_tweets.html', {'tweets': tweets})
 
 def delete(request):
-  try:
-    tweet = ScheduledTweet.get(request.POST.get('key'))
-  except BadKeyError:
-    tweet = None
-  if request.method == 'POST' and tweet and tweet.user == request.user:
+  tweet = _get_untweeted_tweet(request.POST.get('key'), request.user)
+  if request.method == 'POST':
     tweet.delete()
   return HttpResponse('Your Tweet has been deleted.', content_type='text/plain')
+
+
+#====================
+# Assisting functions
+#====================
+def _indicate_twitter_password_stored(user, response):
+  if user.password:
+    # The same cookie is used by image_uploader.js, so remember to change its name
+    # and value there if you do so here, and vice versa. Don't worry about
+    # deleting it or setting it for beyond the browsing session -- both tasks are
+    # handled in Javascript. This is only to prevent the user from being prompted
+    # for his password in the event that his password is stored in the datastore,
+    # but the cookie indicating such has not been set by prior execution of the
+    # associated Javascript.
+    response.set_cookie('twitter_password_stored', 'true')
+
+def _too_many_tweets_sent(user):
+  tweets = ScheduledTweet.all()
+  tweets.filter('user =', user)
+  return tweets.count() >= 5
+
+# Ensures Tweet belogns to given user and that it has not already been Tweeted.
+def _get_untweeted_tweet(key, user):
+  tweet = ScheduledTweet.get(key)
+  if tweet.user != user:
+    raise Exception('User does not own Tweet')
+  if tweet.tweeted:
+    raise Exception('Tweet already Tweeted')
+  return tweet
 
