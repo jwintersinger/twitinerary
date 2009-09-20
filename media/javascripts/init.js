7f41f1a06e8acaaa4ee3642f25f3b71fd93151e6
Jeff Wintersinger <jeff.git@wintersinger.com>
2009-09-20 04:03:16 +0000

    Cache contents of each tab where appropriate.

--- media/javascripts/init.js
+++ media/javascripts/init.js
@@ -13,39 +13,61 @@ function handle_tabs_onload() {
   var notifier = new Notifier('#notifier');
   var tabs = $('#tabs');
 
-  tabs.tabs({load: function(event, ui) {
-    var panel = $(ui.panel);
+  tabs.tabs({
+    // Cache contents of tabs so they aren't reloaded when tab is switched to.
+    // Otherwise, contents of forms will be lost when switching away from and
+    // then back to a tab (not to mention that an unnecessary network request
+    // will be performed, too).
+    cache: true,
+    load: function(event, ui) {
+      return on_tab_load(get_tab_name(ui.tab.id), $(ui.panel), tabs, notifier);
+    },
+    show: function(event, ui) {
+      console.log(event, ui);
+      // "View Tweets" tab must be reloaded to reflect the latest Tweets
+      // added, edited, or deleted -- don't want its contents cached.
+      // TODO: when first loaded and contents aren't already cached, tab is
+      // loaded twice.
+      if(get_tab_name(ui.tab.id) == 'view_tweets')
+        tabs.tabs('load', tabs.tabs('option', 'selected'));
+    }
+  });
+}
 
-    var configure_tweet_editor = function() {
-      var tweet_form = panel.find('.tweet-form');
-      var tweet_input = tweet_form.find('[name=tweet]');
-      new UrlShortener(panel, tweet_input, notifier);
-      new ImageUploader(panel, tweet_input, notifier);
-      return new Tweeter(panel, tweet_form, notifier);
-    };
+function get_tab_name(tab_id) {
+  return tab_id.replace(/_tab$/, '');
+}
 
-    var tabload_handlers = {
-      new_tweet: function() {
-        var tweeter = configure_tweet_editor();
-        tweeter.add_submission_callback(function() { tweeter.reset(); });
-      },
+function on_tab_load(tab_name, panel, tabs, notifier) {
+  var configure_tweet_editor = function() {
+    var tweet_form = panel.find('.tweet-form');
+    var tweet_input = tweet_form.find('[name=tweet]');
+    new UrlShortener(panel, tweet_input, notifier);
+    new ImageUploader(panel, tweet_input, notifier);
+    return new Tweeter(panel, tweet_form, notifier);
+  };
 
-      edit_tweet: function() {
-        var tweeter = configure_tweet_editor();
-        tweeter.add_submission_callback(function() { tabs.tabs('remove', ui.index); });
-      },
+  var tabload_handlers = {
+    new_tweet: function() {
+      var tweeter = configure_tweet_editor();
+      tweeter.add_submission_callback(function() { tweeter.reset(); });
+    },
 
-      view_tweets: function() {
-        new TweetViewer(tabs, notifier);
-        new DatetimeHumanizer();
-      }
-    };
+    edit_tweet: function() {
+      var tweeter = configure_tweet_editor();
+      tweeter.add_submission_callback(function() { tabs.tabs('remove', ui.index); });
+    },
 
-    var tabload_handler = tabload_handlers[ui.tab.id.replace(/_tab$/, '')];
-    // If tab has no ID associated with it, it must be an edit tab, since edit
-    // tabs and panels are created dynamically by jQuery UI, rather than being
-    // hardcoded in my markup.
-    if(!tabload_handler) tabload_handler = tabload_handlers.edit_tweet;
-    tabload_handler();
-  }});
+    view_tweets: function() {
+      new TweetViewer(tabs, notifier);
+      new DatetimeHumanizer();
+    }
+  };
+
+  var tabload_handler = tabload_handlers[tab_name];
+  // If tab has no ID associated with it, it must be an edit tab, since edit
+  // tabs and panels are created dynamically by jQuery UI, rather than being
+  // hardcoded in my markup.
+  if(!tabload_handler) tabload_handler = tabload_handlers.edit_tweet;
+  tabload_handler();
 }
