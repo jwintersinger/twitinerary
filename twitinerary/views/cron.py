3373d65b007507d9d40b5da1e8ee66345262bbfe
Jeff Wintersinger <jeff.git@wintersinger.com>
2009-11-09 22:06:48 +0000

    Add generic exception handler when batch sending Tweets. Now, if one Tweet raises an exception, it will not prevent others from being sent.

--- twitinerary/views/cron.py
+++ twitinerary/views/cron.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import datetime, timedelta
 from twitinerary.models import ScheduledTweet, Twitterer, AuthenticatedUser
 from django.http import HttpResponse
@@ -10,10 +11,16 @@ def batch_send(request):
   tweets.filter('tweeted =', False)
   twitterer = Twitterer()
   for tweet in tweets:
-    # TODO: add e-mail notification (or some other handling) if tweet fails
-    if twitterer.tweet(tweet):
-      tweet.tweeted = True
-      tweet.put()
+    try:
+      successfully_tweeted = twitterer.tweet(tweet)
+    except Exception, e:
+      # TODO: add e-mail notification (or some other handling) if tweet fails. Or perhaps
+      # should simply notify whenever logging.error is called.
+      logging.error('%s raised when sending Tweet %s: %s' % (repr(e), tweet, e))
+    else:
+      if successfully_tweeted:
+        tweet.tweeted = True
+        tweet.put()
   return HttpResponse()
 
 def batch_delete(request):
